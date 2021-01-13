9dbcfc810c25ab09f6fe3d395218221093fecf4a
tanachok12 <tanachok_d@elearning.ac.th>
2021-01-13 23:17:28 +0700

    week5

---
+++ 7-1/main (1).java
@@ -0,0 +1,35 @@
+public class main {
+    public static void main(String[] args) {
+
+        NerfGun gun1 = new NerfGun();
+
+        gun1.fire();
+
+        gun1.reload(15);
+
+        gun1.fire();
+
+        gun1.fire();
+
+        gun1.displayNumOfAmmunition();
+
+        gun1.reload(2);
+
+        gun1.displayNumOfAmmunition();
+
+        gun1.displayGunID();
+
+        NerfGun gun2 = new NerfGun(15);
+
+        gun2.displayGunID();
+
+        gun2.fire();
+
+        gun2.displayNumOfAmmunition();
+
+        gun2.reload(2);
+
+        gun2.displayNumOfAmmunition();
+    }
+}
+
