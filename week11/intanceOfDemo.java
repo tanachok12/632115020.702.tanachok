8e9f9ac3ba4c1bc8cc14a79babe5cd1680b633be
tanachok12 <tanachok_d@elearning.cmu.ac.th>
2021-03-03 22:45:03 +0700

    week11

---
+++ week11/intanceOfDemo.java
@@ -0,0 +1,25 @@
+public  class intanceOfDemo {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        Object c = new c();
+        System.out.println(a instanceof    A);
+        System.out.println(b instanceof B);
+        System.out.println(b instanceof A );
+        System.out.println(a instanceof B);
+        System.out.println(c instanceof  C);
+        System.out.println(c instanceof  A);
+        System.out.println(c instanceof B );
+
+
+    }
+}
+class  A{
+
+}
+class  B extends A{
+
+}
+class C extends B {
+
+}
